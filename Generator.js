b7f23e8ff7587f63573829001384b66da7e4c9ab
ansheww <anshdoshi2305@gmail.com>
2022-12-17 12:04:52 +0530

    generator function changes

--- Generator.js
+++ Generator.js
@@ -1,18 +1,17 @@
-function* generator(){
-    yield 1;
+function* generator(i) {
+    yield i + 1;
     yield 2;
     yield 3;
-    return 'finished'
-};
-const gen=generator();
-for (const iterator of gen) {
-    console.log(iterator);
-    }
-
-
-
-
-// console.log(gen.next());
-// console.log(gen.next());
-// console.log(gen.next());
-// console.log(gen.next());
+    return "finished";
+  }
+  
+  const gen = generator(10);
+//   for (const iterator of gen) {
+//     console.log(iterator);
+//   }
+  
+//   console.log(gen.next().value); //11
+//   console.log(gen.next().value); //2
+//   console.log(gen.next().value); //3
+//   console.log(gen.next().value); //finished
+//   console.log(gen.next().value); //undefined
